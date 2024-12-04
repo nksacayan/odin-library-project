16713763a2673ea39ed84c22485e9f6cc57e072b
Nicholas Sacayan <nksacayan@gmail.com>
2024-12-03 22:15:28 -0800

    whos really lookin here frfr

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "the hobbit",
+        author: "jrr tolksdaddy",
+        numPages: "like a ton",
+        haveRead: "basically"
+    }
+];
 
 function Book(title, author, numPages, haveRead) {
     this.title = title;
@@ -11,18 +18,49 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayAllBooks() {
+function displayBook(book) {
+    const tableRow = document.createElement('tr');
+    tableRow.className = "library-content-row";
+
+    let titleData = document.createElement('td');
+    titleData.content = book.title;
+    tableRow.append(titleData);
+
+    let authorData = document.createElement('td');
+    authorData.content = book.author;
+    tableRow.append(authorData);
+
+    let numPagesData = document.createElement('td');
+    numPagesData.content = book.numPages;
+    tableRow.append(numPagesData);
+
+    let haveReadData = document.createElement('td');
+    haveReadData.content = book.haveRead;
+    tableRow.append(haveReadData);
+
     const libraryTable = document.querySelector("#library-table");
-    // delete all table rows that arent the header row
-    const tableContent = libraryTable.querySelectorAll(":not(.table-header-row)");
-    for (const contentRow in tableContent) {
-        libraryTable.removeChild(contentRow);
-    }
-    // push a row for each book
+    libraryTable.append(tableRow);
 }
 
+function displayLibrary() {
+    for (let book of myLibrary) {
+        displayBook(book);
+    }
+}
 
-const displayLibraryButton = document.querySelector("#display-library-button");
-displayLibraryButton.addEventListener("click", () => {
-    displayAllBooks();
+function clearLibraryContent() {
+    let libraryContent = document.querySelectorAll(".library-content-row");
+    for (let content of libraryContent) {
+        content.remove();
+    }
+}
+
+function onRefreshBooksClicked() {
+    clearLibraryContent();
+    displayLibrary();
+}
+
+const button = document.querySelector("#myButton");
+button.addEventListener("click", () => {
+    onRefreshBooksClicked();
 });
